7f6fcb335cc5de04134b3b5c62e679ecebbea3d7
wangxy <wangxuyang16@crdigital.com.cn>
2024-06-05 10:50:32 +0800

    add base project

---
+++ src/main/java/com/bluekke/activiti8_workflow/Activiti8WorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.bluekke.activiti8_workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activiti8WorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Activiti8WorkflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bluekke/activiti8_workflow/Activiti8WorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluekke.activiti8_workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activiti8WorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
